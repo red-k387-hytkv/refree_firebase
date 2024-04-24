ca4e797c2ce54bb145ce253bf2542c7969cf79a2
barisan387 <red.k387.hytkv@gmail.com>
2024-04-24 09:50:09 +0900

    ローテーション機能実装まで

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ function resetScores() {
     leftScoreBox.value = '0';
     rightScoreBox.value = '0';
 }
+function rotatePlayers(side) {
+    // チームのプレイヤーのIDリストを定義
+    const playerPositions = Array.from(document.querySelectorAll(`.${side}-coat .player`)).map(input => input.id);
+
+    // 最初のプレイヤーを取り出して後でリストの末尾に追加
+    const firstPlayerId = playerPositions.shift();
+
+    // プレイヤーIDをシフトして再割り当て
+    playerPositions.push(firstPlayerId);
+
+    // 元のプレイヤーの値を新しい位置にコピー
+    const playerValues = playerPositions.map(id => document.getElementById(id).value);
+    playerValues.unshift(playerValues.pop()); // 配列の末尾を最初に移動
+
+    // 新しい順序でプレイヤーの値を設定
+    playerPositions.forEach((id, index) => {
+        document.getElementById(id).value = playerValues[index];
+    });
+}
